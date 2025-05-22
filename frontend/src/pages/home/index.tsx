d816f1764b48591f689bb44ca98afbf96be3fef5
Bruno Fabbio <bruno@brunofabbiodev.com>
2025-05-21 23:49:39 -0300

    feat: add register items page

--- frontend/src/pages/home/index.tsx
+++ frontend/src/pages/home/index.tsx
@@ -4,6 +4,7 @@ import { Header } from "@/components/header/Header";
 import { Container } from "@/components/container/Container";
 import { useState, useEffect } from "react";
 import { FiUserPlus } from "react-icons/fi";
+import Link from "next/link"
 
 interface babyProps {
   id: number;
@@ -72,8 +73,10 @@ export default function Home() {
                         <p className="text-gray-700 text-sm">{baby.head_circumference}</p>
                     </div>
                     <button className="w-full inline-flex justify-center mt-1 rounded-md border bg-background px-2 py-2 text-sm font-medium shadow-sm cursor-pointer hover:text-blue-600 transition-all duration-300">
-                        <FiUserPlus className="h-4 w-4 mr-2" />
-                        Registrar Informações
+                        <Link href={`/registerbaby/${baby.id}`} className="flex items-center w-full justify-center">
+                          <FiUserPlus className="h-4 w-4 mr-2" />
+                          Registrar Informações
+                        </Link>
                     </button>
                 </div>
               </div>
