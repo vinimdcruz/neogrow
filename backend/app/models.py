e0971648386896c0cea0240d362b932a56c90b9d
Vinicius Cruz <vmcruz89@gmail.com>
2025-07-10 22:29:37 -0300

    feat: add timestamps fields; update deploy workflow to run migrations (if exists) on every deploy

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,5 +1,6 @@
-from sqlalchemy import Column, Integer, Float, String, Date, ForeignKey, Boolean
+from sqlalchemy import Column, Integer, Float, String, Date, ForeignKey, Boolean, DateTime
 from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
 from app.database import Base
 
 class User(Base):
@@ -10,6 +11,9 @@ class User(Base):
     username = Column(String, unique=True, index=True)
     hashed_password = Column(String)
     is_active = Column(Boolean, default=True)
+    created_at = Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
+    updated_at = Column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now(), nullable=False)
+    deleted_at = Column(DateTime(timezone=True), nullable=True)
     babies = relationship("Baby", back_populates="user")
 
 class Baby(Base):
@@ -19,6 +23,9 @@ class Baby(Base):
     name = Column(String, nullable=False)
     birth_date = Column(Date, nullable=False)
     user_id = Column(Integer, ForeignKey('users.id'))
+    created_at = Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
+    updated_at = Column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now(), nullable=False)
+    deleted_at = Column(DateTime(timezone=True), nullable=True)
     user = relationship("User", back_populates="babies")
     growth_data = relationship("BabyData", back_populates="baby", cascade="all, delete-orphan")
 
@@ -31,4 +38,7 @@ class BabyData(Base):
     head_circumference = Column(Float, nullable=False)
     date = Column(Date, nullable=False)
     baby_id = Column(Integer, ForeignKey('babies.id'))
+    created_at = Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
+    updated_at = Column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now(), nullable=False)
+    deleted_at = Column(DateTime(timezone=True), nullable=True)
     baby = relationship("Baby", back_populates="growth_data")
