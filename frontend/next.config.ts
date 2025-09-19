163f3615b9739f19480b22715b2b9f0ac3a93857
Vinicius Cruz <vmcruz89@gmail.com>
2025-09-18 23:14:29 -0300

    fix: remove redundant api string

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -18,7 +18,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: `${process.env.NEXT_PUBLIC_API_URL}/api/:path*`,
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`,
       },
     ];
   },
